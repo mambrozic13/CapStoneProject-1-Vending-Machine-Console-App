85c3cfb53e293c2fcd5d0fd17b740d32b6a5af8f
Mike Morel <mike@techelevator.com>
2019-06-05 20:51:51 -0400

    Add CLI Menu classes

--- 18_Capstone/Capstone/Program.cs
+++ 18_Capstone/Capstone/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Views;
+using System;
 
 namespace Capstone
 {
@@ -6,7 +7,11 @@ namespace Capstone
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // If you want to use the CLI menu, you can create an instance in Main, and 
+            // Run it.  You can customize the Main menu, and create other menus in the Views folder.
+            // If you do not want to use the CLI menu, you can delete the files from the Views folder.
+            MainMenu menu = new MainMenu();
+            menu.Run();
         }
     }
 }
